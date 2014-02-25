6d18fa4ce75d114c3e572c9c2e17a63bca6c4180
Victor <victordahlin@gmail.com>
2014-02-25 09:53:03 +0100

    Created method instead of view code in controller

--- js/view/SelectDishController.js
+++ js/view/SelectDishController.js
@@ -29,58 +29,6 @@ var SelectDishController = function(view, model ) {
 		var dishDesc = dish.description;
 		var dishIngredients = dish.ingredients; // array
 
-		var masterDiv = $("<div>");
-
-		var left = $("<div>");
-		left.addClass("col-md-4");
-
-		var title = $("<h2>");
-		title.html(dishName);
-		left.append(title);
-
-		var imageTag = $("<img>");
-		var file = "images/" + dishImage;
-		imageTag.attr("src",file);
-		imageTag.attr("width", "120");
-		imageTag.attr("height", "120");
-		left.append(imageTag);
-
-		var dishText = $("<p>");
-		dishText.html(dishDesc);
-		left.append(dishText);
-
-		var leftBackButton = $("<button>");
-		leftBackButton.html("back to Select Dish");
-		left.append(leftBackButton);
-
-		// Add first div to master
-		masterDiv.append(left);
-
-		var right = $("<div>");
-		right.addClass("col-md-6");
-		var dishIngrTotal = $("<h3>");
-		dishIngrTotal.html("Ingredients for " + model.getNumberOfGuests() + " people<hr>");
-		right.append(dishIngrTotal);
-
-		for(key in dishIngredients) {
-			var singleIngredient = $("<p>");
-			var ingrName = dishIngredients[key].name;
-			var ingrQuant = dishIngredients[key].quantity;
-			var ingrUnit = dishIngredients[key].unit;
-			var ingrPrice = dishIngredients[key].price;
-
-			singleIngredient.html(ingrQuant + " " + ingrUnit + " " + ingrName + " SEK  " + ingrPrice);
-			right.append(singleIngredient);
-		}
-
-		var breakLine = $("<hr>")
-		right.append(breakLine);
-		var rightConfirmButton = $("<button>");
-		rightConfirmButton.html("Confirm Dish");
-		right.append(rightConfirmButton);
-		masterDiv.append(right);
-
-		// Set all HTML tags to the #picture-box in #selectDish
-		$("#showDish").append(masterDiv);
+		view.test(dishID, dishName, dishImage, dishDesc, dishIngredients);
 	});
 }
--- js/view/selectDish.js
+++ js/view/selectDish.js
@@ -47,6 +47,62 @@ var SelectDish = function (container,model) {
 			pictureBoxTag.append(div);
 		}
 	}
+
+	this.test = function(dishID, dishName, dishImage, dishDesc, dishIngredients) {
+				var masterDiv = $("<div>");
+
+		var left = $("<div>");
+		left.addClass("col-md-4");
+
+		var title = $("<h2>");
+		title.html(dishName);
+		left.append(title);
+
+		var imageTag = $("<img>");
+		var file = "images/" + dishImage;
+		imageTag.attr("src",file);
+		imageTag.attr("width", "120");
+		imageTag.attr("height", "120");
+		left.append(imageTag);
+
+		var dishText = $("<p>");
+		dishText.html(dishDesc);
+		left.append(dishText);
+
+		var leftBackButton = $("<button>");
+		leftBackButton.html("back to Select Dish");
+		left.append(leftBackButton);
+
+		// Add first div to master
+		masterDiv.append(left);
+
+		var right = $("<div>");
+		right.addClass("col-md-6");
+		var dishIngrTotal = $("<h3>");
+		dishIngrTotal.html("Ingredients for " + model.getNumberOfGuests() + " people<hr>");
+		right.append(dishIngrTotal);
+
+		for(key in dishIngredients) {
+			var singleIngredient = $("<p>");
+			var ingrName = dishIngredients[key].name;
+			var ingrQuant = dishIngredients[key].quantity;
+			var ingrUnit = dishIngredients[key].unit;
+			var ingrPrice = dishIngredients[key].price;
+
+			singleIngredient.html(ingrQuant + " " + ingrUnit + " " + ingrName + " SEK  " + ingrPrice);
+			right.append(singleIngredient);
+		}
+
+		var breakLine = $("<hr>")
+		right.append(breakLine);
+		var rightConfirmButton = $("<button>");
+		rightConfirmButton.html("Confirm Dish");
+		right.append(rightConfirmButton);
+		masterDiv.append(right);
+
+		// Set all HTML tags to the #picture-box in #selectDish
+		$("#showDish").append(masterDiv);
+	}
 	
 	this.fillDishContainer("starter");
 
